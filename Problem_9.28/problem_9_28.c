e7089be6740ee595803675aea83f1ea53ac0b290
Sujay <58305810+Sujay1101@users.noreply.github.com>
2021-11-15 16:42:12 +0530

    Update problem_9_28.c
    Added comments

--- Problem_9.28/problem_9_28.c
+++ Problem_9.28/problem_9_28.c
@@ -1,3 +1,5 @@
+//Program to find the offset and page number of a virtual address
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -7,15 +9,18 @@
 
 #define PAGE_SIZE 4096
 
+//Helper functions
 bool is_virtual_address_valid(char *);
 bool are_all_characters_digits(char *);
 bool fits_in_32_bits(char *);
 
 int main(int argc, const char *argv[])
 {
+        //Virtual address, page number and offset respectively
         unsigned int va;
         unsigned int pn;
         unsigned int o;
+        //Variable to hold virtual address
         char *vas = (char *)argv[1];
         bool e;
         
@@ -26,7 +31,7 @@ int main(int argc, const char *argv[])
                 return 1;
         }
 
-
+        //Running loop till user wishes to exit or valid virtual address entered
         while(is_virtual_address_valid(vas) == false)
         {
 
@@ -41,7 +46,9 @@ int main(int argc, const char *argv[])
                         return 1;
          }
         
+        //Converting virtual address data type from string to int 
         va = atoi(vas);
+        //Finding offset and page number respectively
         o = va % PAGE_SIZE;
         pn = va / PAGE_SIZE;
 
@@ -68,8 +75,10 @@ bool are_all_characters_digits(char * va)
 {
         const char *error = "Atleast one character entered is not a digit from 0 to 9";
         unsigned int l = strlen(va);
+        //Checking whether every character is a digit in input string
         for(int i = 0; i != l; i++)
         {
+                //If not digit print error and return false 
                 if(isdigit(va[i]) == false)
                 {
                         printf("%s\n", error);
@@ -81,8 +90,9 @@ bool are_all_characters_digits(char * va)
 
 bool fits_in_32_bits(char *va)
 {       const char *error = "Virtual address is more than 32 bits long";
+        //Converting input string to 64 bit value to safely compare with 2^32 - 1
         unsigned long int temp = atol(va);
-        if(temp > pow(2, 32))
+        if(temp > pow(2, 32) - 1)
         {
                 printf("%s\n", error);
                 return false;
